b5187899556c30dd0b7c4efd0625e7804d652e3d
goldsborough <peter@goldsborough.me>
2017-03-16 20:14:46 +0000

    Documented context-data and declaration-data

--- include/clang-expand/common/context-data.hpp
+++ include/clang-expand/common/context-data.hpp
@@ -12,12 +12,21 @@
 
 namespace ClangExpand {
 
+/// Stores information about a context (namespace, class name etc.).
+///
+/// C++ allows classes inside namespaces to have the same name as the namespace
+/// (`namespace X { struct X {}; }`), so we can't just store the name, we also
+/// need the `Decl::Kind`.
 struct ContextData {
+  /// Constructor.
   ContextData(clang::Decl::Kind kind_, const llvm::StringRef& name_)
   : kind(kind_), name(name_.str()) {
   }
 
+  /// The `clang::Decl::Kind` of the context (usually CXXRecord or Namespace).
   clang::Decl::Kind kind;
+
+  /// The name of the context (class, namespace etc.).
   std::string name;
 };
 }  // namespace ClangExpand
--- include/clang-expand/common/declaration-data.hpp
+++ include/clang-expand/common/declaration-data.hpp
@@ -14,19 +14,75 @@
 #include <string>
 
 namespace ClangExpand {
+/// Stores information about a function declaration.
+///
+/// This information is used to uniquely identify any function that we might
+/// look for. Note that the function could actually be a constructor or operator
+/// overload.
 struct DeclarationData {
   using ParameterMap = llvm::StringMap<std::string>;
 
+  /// Constructor.
   explicit DeclarationData(const std::string& name_,
                            const Location& location_)
   : name(name_), location(location_) {
   }
 
+  /// The name of the function (or operator).
   std::string name;
+
+  /// The raw source text of the entire function declaration.
+  ///
+  /// If the declaration is also a definition, this will include the definition.
   std::string text;
+
+  /// The contexts of the function (namespaces, class names etc.).
+  ///
+  /// The order of these contexts is from most nested to least nested, i.e.
+  /// given a function, its contexts can be compared from "the inside out". For
+  /// example, given this function definition:
+  ///
+  /// ```.cpp
+  /// namesapce A {
+  /// namespace B {
+  /// class C {
+  ///   void f();
+  /// };
+  /// }
+  /// }
+  /// ```
+  ///
+  /// The contexts will be 'C', 'B', 'A' (in this order!).
   llvm::SmallVector<ContextData, 8> contexts;
+
+  /// The types of the parameters as they appear in the function definition.
+  ///
+  /// The type strings stored are fully qualified to retain as much information
+  /// as possible across for serialization between symbol and definition search.
+  /// More precisely, the type information is retrieved through:
+  /// `parameter->getOriginalType().getCanonicalType().getAsString(policy)`,
+  /// where `parameter` is a `ParmVarDecl` and `policy` is an appropriate
+  /// `PrintingPolicy` retrieved from the `ASTContext` that stores the full
+  /// qualification of the name.
   llvm::SmallVector<std::string, 8> parameterTypes;
+
+  /// The mapping from parameter names to respective argument expressions.
+  ///
+  /// For example, given this function and call:
+  /// ```.cpp
+  /// void f(int x, double y);
+  /// f(1, g(2));
+  /// ```
+  ///
+  /// where `g` is some double-valued function, the mapping will be:
+  /// - x -> 1
+  /// - y -> g(2)
+  /// such that during rewriting, every ocurrence of `x` and `y` inside the
+  /// function can be replaced with `1` and `g(2)`, respectively.
   ParameterMap parameterMap;
+
+  /// The location of the function declaration (right at the name of the
+  /// function).
   Location location;
 };
 }  // namespace ClangExpand
